c4a8610e42a366afd183982475c635603521495d
Eduarda Souto <eduardasouto2823@gmail.com>
2023-05-22 20:18:12 -0300

    fix: permite qualquer origem de solicitação

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const client = require('./db').default;
 const List = require('./api/List');
 const Item = require('./api/Item');
